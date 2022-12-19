5181fb1c3076f5510a3a1ab2bfdf4dbdbf65c159
sittingpretty <chesler9@gmail.com>
2022-12-19 11:54:54 -0500

    Added table constraint

--- SQL examples/practice.sql
+++ SQL examples/practice.sql
@@ -1,9 +1,14 @@
 CREATE DATABASE calendarInfo;
 USE calendarInfo;
 CREATE TABLE customerInfo (
-    customerID INT UNSIGNED;
-    customerName VARCHAR(80);
-    customerAddress VARCHAR(150);
-    customerEmail VARCHAR(65);
-    
-)
+    customerID: INT UNSIGNED PRIMARY KEY,
+    customerName: VARCHAR(80) NOT NULL,
+    customerAddress: VARCHAR(150) NOT NULL,
+    customerEmail: VARCHAR(65),
+    customerPhone: INT(10) NOT NULL,
+    customerRegion: VARCHAR(25) NOT NULL,
+    productType: VARCHAR(35) NOT NULL,
+    productCode: INT UNSIGNED NOT NULL,
+    dateCreated: TIMESTAMP NOT NULL DEFAULT NOW(),
+    CONSTRAINT cID UNIQUE (customerID, customerName)
+);
